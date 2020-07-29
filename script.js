1c19cea36fd3ff9bd569d28be565336421495ddd
Sakiskid <tylerdrew.home@gmail.com>
2020-07-29 12:36:15 -0500

    Starting new index made with bootstrap, so I can create a custom form easier

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  
+  return password;
 }
 
 // Add event listener to generate button
